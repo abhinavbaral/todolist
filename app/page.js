fa4a3c80c8c030a29f07356ae200f467cd78dae5
Abhinav Baral <abhinavbaral@gmail.com>
2024-09-15 17:10:27 +0545

    Add notes and show notes

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+"use client"
 import React, { useState } from 'react'
 import Header from '@/Components/Header'
 import Footer from '@/Components/Footer'
@@ -5,16 +6,28 @@ import Footer from '@/Components/Footer'
 // This is the About Us page.
 
 export const page = () => {
-  const [first, setfirst] = useState(second)
+  const [first, setfirst] = useState("")
+  const [MTask, setMTask] = useState([])
+  let handler = function(e){
+    e.preventDefault ()
+    setfirst("")
+  }
+  let show = <h2>No Notes</h2>
   return (
     <>
     <Header/>
-
-    <form className='text-2xl text-black font-light  m-5 '>
-  <input type='text' placeholder='Add a new Note' />
+    <form className='text-2xl text-black font-light  m-5 ' onSubmit={handler}>
+  <input type='text' placeholder='Add a new Note' value={first} onChange={function(e){
+setfirst(e.target.value)
+  }} />
   <button className='bg-black m-5 px-2 py-.5 text-white text-2xl rounded'>Add</button>
 </form>
-
+<hr/>
+<div className='p-8 bg-slate-200'>
+  <ul>
+    {show}
+  </ul>
+</div>
     <Footer/>
     </>
   )
