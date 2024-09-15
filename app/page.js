44e68548e224243cecb4497a5ac398827d0dbbe0
Abhinav Baral <abhinavbaral@gmail.com>
2024-09-15 16:01:33 +0545

    Js updates rafc usage

--- app/page.js
+++ app/page.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 export const page = () => {
   return (
-    <div>page</div>
+    <>
+    <Header/>
+    <h1>About Us</h1>
+    <p>This is the About Us page.</p>
+    </>
   )
 }
