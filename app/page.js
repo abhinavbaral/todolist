6314e30afc8dfd49bc592e4593142f84282233ac
Abhinav Baral <abhinavbaral@gmail.com>
2024-09-15 17:23:52 +0545

    update code

--- app/page.js
+++ app/page.js
@@ -5,37 +5,49 @@ import Footer from '@/Components/Footer'
 
 // This is the About Us page.
 
-export const page = () => {
-  const [first, setfirst] = useState("")
+export const Page = () => {
+  const [first, setFirst] = useState("")
   const [MNote, setMNote] = useState([])
-  setMNote([...MNote, {first}])
-  let handler = function(e){
-    e.preventDefault ()
-    setfirst("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (first.trim() !== "") {
+      setMNote([...MNote, { first }])
+      setFirst("")
+    }
   }
-  let show = <h2>No Notes</h2>
-  show= MNote.map(function (Note,index) {
-    return
-    <h5>{Note.first}</h5>
-    
-  })
+
   return (
     <>
-    <Header/>
-    <form className='text-2xl text-black font-light  m-5 ' onSubmit={handler}>
-  <input type='text' placeholder='Add a new Note' value={first} onChange={function(e){
-setfirst(e.target.value)
-  }} />
-  <button className='bg-black m-5 px-2 py-.5 text-white text-2xl rounded'>Add</button>
-</form>
-<hr/>
-<div className='p-8 bg-slate-200'>
-  <ul>
-    {show}
-  </ul>
-</div>
-    <Footer/>
+      <Header />
+      <form className='text-2xl text-black font-light m-5' onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='Add a new Note'
+          value={first}
+          onChange={(e) => setFirst(e.target.value)}
+        />
+        <button className='bg-black m-5 px-2 py-0.5 text-white text-2xl rounded' type='submit'>
+          Add
+        </button>
+      </form>
+      <hr />
+      <div className='p-8 bg-slate-200'>
+        <ul>
+          {MNote.length === 0 ? (
+            <h2>No Notes</h2>
+          ) : (
+            MNote.map((note, index) => (
+              <li key={index}>
+                <h5>{note.first}</h5>
+              </li>
+            ))
+          )}
+        </ul>
+      </div>
+  
     </>
   )
 }
-export default page
+
+export default Page
