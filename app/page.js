e714371bfa4083e3d1a40ddabeacc270e559faf4
Abhinav Baral <abhinavbaral@gmail.com>
2024-09-17 17:05:13 +0545

    working delete

--- app/page.js
+++ app/page.js
@@ -1,40 +1,40 @@
 "use client";
 import React, { useState } from "react";
 import Header from "@/Components/Header";
-import 'remixicon/fonts/remixicon.css';
-
-
+import "remixicon/fonts/remixicon.css";
 
 export const page = () => {
   const [Inp, setInp] = useState("");
-  const [MNote, setMNote] = useState([])
+  const [MNote, setMNote] = useState([]);
 
-  let note = <h2>
-  Add a new Note
-    </h2>;
-    if(MNote. length>0) {
-      note = MNote.map((Note,index) =>{
-        return(
-          <li className="flex justify-between">
-                  <div className="flex items-center justify-between mb-5">
-          <h5 className="text-2xl font-semibold"> 
-           {Note.Inp}
-          </h5>
-        </div> 
-        <button className="bg-red-500 text-white  items-center justify-center text-center px-2 py-2 m-3 rounded">
-        <i class="ri-delete-bin-6-fill"></i>
-        </button>
-          </li>
-        )
-      })
-      
-    }
-  
-  let Handle = function (e){
-     e.preventDefault()
-     setMNote([...MNote, {Inp}]);
-     setInp("")
+  let DHandle = function (index){
+let Copy = [...MNote]
+Copy.splice(index,1)
+setMNote(Copy)
+}
+
+  let note = <h2>Add a new Note</h2>;
+  if (MNote.length > 0) {
+    note = MNote.map((Note, index) => {
+      return (
+        <li key={index} className="flex justify-between">
+          <div className="flex items-center justify-between mb-5">
+            <h5 className="text-2xl font-semibold">{Note.Inp}</h5>
+          </div>
+          <button onClick={function(){DHandle(index)}}
+          className="bg-red-500 text-white  items-center justify-center text-center px-2 py-2 m-3 rounded">
+            <i class="ri-delete-bin-6-fill"></i>
+          </button>
+        </li>
+      );
+    });
   }
+
+  let Handle = function (e) {
+    e.preventDefault();
+    setMNote([...MNote, { Inp }]);
+    setInp("");
+  };
   return (
     <>
       <Header />
@@ -44,21 +44,21 @@ export const page = () => {
           className="border-2 border-black m-5 px-4 py-2"
           placeholder="Enter your Note"
           value={Inp}
-          onChange={function(e){
-             setInp(e.target.value)
+          onChange={function (e) {
+            setInp(e.target.value);
           }}
         />
-        <button onClick={Handle} className="bg-green-500 text-white  items-center justify-center text-center px-4 py-2 m-5 rounded">
-        <i class="ri-add-line"></i>
+        <button
+          onClick={Handle}
+          className="bg-green-500 text-white  items-center justify-center text-center px-4 py-2 m-5 rounded"
+        >
+          <i class="ri-add-line"></i>
         </button>
       </form>
-      <hr/>    
+      <hr />
       <div className="p-8 bg-gray-200">
-        <ul>
-            {note}
-
-        </ul>
-        </div>
+        <ul>{note}</ul>
+      </div>
     </>
   );
 };
